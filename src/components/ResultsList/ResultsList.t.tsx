b98524495597bb11140e6d9da1ef936a0748fa94
vwital <wital128@tut.by>
2024-07-29 00:37:58 +0300

    feat: rename test files for push

--- src/components/ResultsList/ResultsList.t.tsx
+++ src/components/ResultsList/ResultsList.t.tsx
@@ -1,4 +1,3 @@
-// import '@testing-library/jest-dom';
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
