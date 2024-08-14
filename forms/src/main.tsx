db71c793c6f120d8e5d7f54dc0028b7f8063428a
vwital <wital128@tut.by>
2024-08-14 22:20:17 +0300

    feat: add react-form markup

--- forms/src/main.tsx
+++ forms/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ReactForm from './pages/ReactForm/ReactForm.tsx';
+import UncontrolledForm from './pages/UncontrolledForm/UncontrolledForm.tsx';
 import './index.css';
+
+const router = createBrowserRouter([
+    { path: '/', element: <App /> },
+    { path: '/react-form', element: <ReactForm /> },
+    { path: '/uncontrolled-form', element: <UncontrolledForm /> },
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>
 );
---
+++ forms/src/pages/UncontrolledForm/UncontrolledForm.tsx
@@ -0,0 +1,11 @@
+function UncontrolledForm() {
+    return (
+        <>
+            <h1>Uncontrolled Form</h1>
+            <input type="text"></input>
+            <button>Submit</button>
+        </>
+    );
+}
+
+export default UncontrolledForm;
